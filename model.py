1c3b5ab771722929959f02f67b1357b20a14e798
Sai Narayanam <35761274+imsrinin@users.noreply.github.com>
2023-04-26 19:54:52 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -14,24 +14,11 @@ import torch.nn.utils.prune as prune
 import numpy as np
 import time
 from typing import Type, Any, Callable, Union, List, Optional
-import copy
 
-
-###################################################################################################################
 
 model_urls = {
     'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
 }
-
-def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=dilation, groups=groups, bias=False, dilation=dilation)
-
-
-def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
 class BasicBlock(nn.Module):
@@ -42,26 +29,15 @@ class BasicBlock(nn.Module):
         inplanes: int,
         planes: int,
         stride: int = 1,
-        downsample: Optional[nn.Module] = None,
-        groups: int = 1,
-        base_width: int = 64,
-        dilation: int = 1,
-        norm_layer: Optional[Callable[..., nn.Module]] = None) -> None:
+        downsample: Optional[nn.Module] = None) -> None:
 
         super(BasicBlock, self).__init__()
-
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        if groups != 1 or base_width != 64:
-            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
-        if dilation > 1:
-            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
         
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
+        self.conv1  = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
         self.skip_add = nn.quantized.FloatFunctional()
@@ -87,12 +63,6 @@ class BasicBlock(nn.Module):
 
 
 class Bottleneck(nn.Module):
-    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
-    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
-    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
-    # This variant is also known as ResNet V1.5 and improves accuracy according to
-    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
-
     expansion: int = 4
 
     def __init__(
@@ -100,29 +70,20 @@ class Bottleneck(nn.Module):
         inplanes: int,
         planes: int,
         stride: int = 1,
-        downsample: Optional[nn.Module] = None,
-        groups: int = 1,
-        base_width: int = 64,
-        dilation: int = 1,
-        norm_layer: Optional[Callable[..., nn.Module]] = None) -> None:
+        downsample: Optional[nn.Module] = None) -> None:
 
         super(Bottleneck, self).__init__()
 
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-
-        width = int(planes * (base_width / 64.)) * groups
-
-        self.conv1 = conv1x1(inplanes, width)
-        self.bn1 = norm_layer(width)
-        self.conv2 = conv3x3(width, width, stride, groups, dilation)
-        self.bn2 = norm_layer(width)
-        self.conv3 = conv1x1(width, planes * self.expansion)
-        self.bn3 = norm_layer(planes * self.expansion)
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, stride=stride, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
-        self.skip_add = nn.quantized.FloatFunctional()
+        self.ff = nn.quantized.FloatFunctional() # to simulate qunatized operations on float tensors (https://pytorch.org/docs/stable/generated/torch.ao.nn.quantized.FloatFunctional.html)
 
     def forward(self, x: Tensor) -> Tensor:
         identity = x
@@ -138,7 +99,7 @@ class Bottleneck(nn.Module):
         if self.downsample is not None:
             identity = self.downsample(x)
 
-        out = self.skip_add.add(out, identity)
+        out = self.ff.add(out, identity)# (https://pytorch.org/docs/stable/generated/torch.ao.nn.quantized.FloatFunctional.html)
         out = self.relu(out)
 
         return out
@@ -151,40 +112,24 @@ class ResNet(nn.Module):
         block: Type[Union[BasicBlock, Bottleneck]],
         layers: List[int],
         num_classes: int = 1000,
-        zero_init_residual: bool = False,
-        groups: int = 1,
-        width_per_group: int = 64,
-        replace_stride_with_dilation: Optional[List[bool]] = None,
-        norm_layer: Optional[Callable[..., nn.Module]] = None
-    ) -> None:
+        ) -> None:
+
         super(ResNet, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self._norm_layer = norm_layer
 
         self.inplanes = 64
-        self.dilation = 1
-        if replace_stride_with_dilation is None:
-            replace_stride_with_dilation = [False, False, False]
 
-        if len(replace_stride_with_dilation) != 3:
-            raise ValueError("replace_stride_with_dilation should be None "
-                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
-        
-        self.groups = groups
-        self.base_width = width_per_group
         self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
                                bias=False)
-        self.bn1 = norm_layer(self.inplanes)
+        self.bn1 = nn.BatchNorm2d(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
-                                       dilate=replace_stride_with_dilation[0])
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
-                                       dilate=replace_stride_with_dilation[1])
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
-                                       dilate=replace_stride_with_dilation[2])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
@@ -195,45 +140,31 @@ class ResNet(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
+        # stubs to aid qunatization process
         self.quant = QuantStub()
         self.dequant = DeQuantStub()
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)  # type: ignore[arg-type]
-                elif isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
 
     def _make_layer(self, block: Type[Union[BasicBlock, Bottleneck]], planes: int, blocks: int,
-                    stride: int = 1, dilate: bool = False) -> nn.Sequential:
-        norm_layer = self._norm_layer
+                    stride: int = 1) -> nn.Sequential:
+
         downsample = None
-        previous_dilation = self.dilation
-        if dilate:
-            self.dilation *= stride
-            stride = 1
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
-                            self.base_width, previous_dilation, norm_layer))
+        layers.append(block(self.inplanes, planes, stride, downsample))
         self.inplanes = planes * block.expansion
         for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, groups=self.groups,
-                                base_width=self.base_width, dilation=self.dilation,
-                                norm_layer=norm_layer))
+            layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
 
-    def _forward_impl(self, x: Tensor) -> Tensor:
 
+    def forward(self, x):
+        # adding quant stub to indicate qunatization start
         x = self.quant(x) 
 
         x = self.conv1(x)
@@ -249,40 +180,26 @@ class ResNet(nn.Module):
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
-
+        # adding dequant stub to indicate qunatization end
         x = self.dequant(x) 
 
         return x
 
-    def forward(self, x: Tensor) -> Tensor:
-        return self._forward_impl(x)
 
-def _resnet(
-    arch: str,
-    block: Type[Union[BasicBlock, Bottleneck]],
-    layers: List[int],
-    pretrained: bool,
-    progress: bool,
-    **kwargs: Any
-) -> ResNet:
-    model = ResNet(block, layers, **kwargs)
+def qResNet50(
+    arch: str = 'resnet50',
+    pretrained: bool = True, 
+    progress: bool = True, **kwargs: Any) -> ResNet:
+
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls[arch],
                                               progress=progress)
         model.load_state_dict(state_dict)
+
     return model
 
-
-def qResNet50(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> ResNet:
-    r"""ResNet-50 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _resnet('resnet50', Bottleneck, [3, 4, 6, 3], pretrained, progress,
-                   **kwargs)
-###################################################################################################################
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -298,8 +215,6 @@ classes = ('plane', 'car' , 'bird',
 transform = transforms.Compose(
     [transforms.RandomHorizontalFlip(),
      transforms.RandomCrop(32, padding=4),
-    #  transforms.RandomRotation(15),
-    #  transforms.RandomVerticalFlip(),
      transforms.ToTensor(),
      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010) )])
 
@@ -317,10 +232,12 @@ train_loader = torch.utils.data.DataLoader(train_dataset
     , batch_size = batch_size
     , shuffle = True,
     num_workers=2)
+
 test_loader = torch.utils.data.DataLoader(test_dataset
     , batch_size = batch_size
     , shuffle = True,
     num_workers=2)
+
 n_total_step = len(train_loader)
 
 
@@ -328,30 +245,6 @@ n_total_step = len(train_loader)
 model = qResNet50(pretrained=True)
 model.fc = nn.Linear(model.fc.in_features, 10)
 
-
-class QuantizedResNet18(nn.Module):
-    def __init__(self, model_fp32):
-        super(QuantizedResNet18, self).__init__()
-        # QuantStub converts tensors from floating point to quantized.
-        # This will only be used for inputs.
-        self.quant = torch.quantization.QuantStub()
-        # DeQuantStub converts tensors from quantized to floating point.
-        # This will only be used for outputs.
-        self.dequant = torch.quantization.DeQuantStub()
-        # FP32 model
-        self.model_fp32 = model_fp32
-
-    def forward(self, x):
-        # manually specify where tensors will be converted from floating
-        # point to quantized in the quantized model
-        x = self.quant(x)
-        x = self.model_fp32(x)
-        # manually specify where tensors will be converted from quantized
-        # to floating point in the quantized model
-        x = self.dequant(x)
-        return x
-    
-# model = QuantizedResNet18(model_)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate, momentum=0.9,weight_decay=5e-4)
@@ -366,22 +259,10 @@ def prune_model(model, pruning_rate, mode):
             if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
                 parameters_to_prune.append((module, 'weight'))
 
-
         prune.global_unstructured(
             parameters_to_prune,
             pruning_method=prune.L1Unstructured,
             amount=pruning_rate)
-
-    if mode == 'global_structured':
-            parameters_to_prune = []
-            for name, module in model.named_modules():
-                if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
-                    parameters_to_prune.append((module, 'weight'))
-
-            prune.global_unstructured(
-                parameters_to_prune,
-                pruning_method=prune.LnStructured,
-                amount=pruning_rate)
 
     if mode == 'local_unstructured':
         for name, module in model.named_modules():
@@ -391,17 +272,14 @@ def prune_model(model, pruning_rate, mode):
     if mode == 'local_structured':
         for name, module in model.named_modules():
             if isinstance(module, nn.Conv2d):
-                # prune.ln_structured(module, 'weight', amount=pruning_rate)
                 prune.ln_structured(module, name='weight', amount=pruning_rate, n=2, dim=0)
-
-
 
     return model
 
 model.train()
 model.to(device)
-model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
-torch.quantization.prepare_qat(model,inplace=True)
+# model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
+# torch.quantization.prepare_qat(model,inplace=True)
 start_time = time.time()
 
 
@@ -411,7 +289,7 @@ for i in range(1):
 
     # print("Training pruned model...")
 
-    for epoch in range(100):
+    for epoch in range(1):
         # if epoch == 90:
             # model = prune_model(model, 0.94, mode = 'local_unstructured')      
             # print("Training pruned model...")
@@ -430,11 +308,9 @@ for i in range(1):
                 print(f'epoch {epoch+1}/{num_epochs}, step: {batch_idx+1}/{n_total_step}: loss = {loss:.5f}, acc = {100*(n_corrects/target.size(0)):.2f}%')
             
 
-
-
 # for name, module in model.named_modules():
-#         # if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
-#         if isinstance(module, nn.Conv2d):
+#         if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+#         # if isinstance(module, nn.Conv2d):
 #             prune.remove(module, 'weight')
 
 end_time = time.time()
@@ -442,17 +318,14 @@ end_time = time.time()
 
 print('total time taken for training iterative pruning model in cuda is : ', (end_time-start_time))
 
-# torch.save(model.state_dict(), 'model.pth')
 
-# model = qResNet50()
-# model.load_state_dict(torch.load('model.pth'))
 
 model.to('cpu')
 
 model.eval()
-torch.quantization.convert(model, inplace = True)
+# torch.quantization.convert(model, inplace = True)
 
-#inference latency
+# #inference latency
 inf_base_start = time.time()
 
 with torch.no_grad():
@@ -474,13 +347,6 @@ print('total time taken for inference in cpu for pruned model is : ', (inf_base_
 
 # model.eval()
 
-# # model.qconfig = torch.quantization.QConfig(activation=torch.quantization.MinMaxObserver.with_args(dtype=torch.quint4x2, qscheme=torch.per_tensor_symmetric),
-# #                                     weight=torch.quantization.MinMaxObserver.with_args(dtype=torch.quint4x2, qscheme=torch.per_tensor_symmetric))
-# # model = torch.quantization.quantize_dynamic(
-# #     model,  # the model to be quantized
-# #     {torch.nn.Conv2d, torch.nn.Linear},
-# #      dtype=torch.qint8
-# # )
 # model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
 
 # torch.quantization.prepare(model, inplace = True)
@@ -502,7 +368,7 @@ print('total time taken for inference in cpu for pruned model is : ', (inf_base_
 #         y_predicted = model(test_images_set)
 
 
-# torch.quantization.convert(model, inplace = True) # Quantize the model
+# torch.quantization.convert(model, inplace = True) 
 
 # model.eval()
 
@@ -514,7 +380,6 @@ print('total time taken for inference in cpu for pruned model is : ', (inf_base_
 #     for i, (test_images_set , test_labels_set) in enumerate(test_loader):
 #         test_images_set = test_images_set.to('cpu')
 #         test_labels_set = test_labels_set.to('cpu')
-    
 #         y_predicted = model(test_images_set)
 #         labels_predicted = y_predicted.argmax(axis = 1)
 #         number_corrects += (labels_predicted==test_labels_set).sum().item()
